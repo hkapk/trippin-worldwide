b60b1494b6f4253dafb887ef35663484c6c34f9b
kngurley <kngurley25@gmail.com>
2021-12-21 22:10:43 -0600

    add checkpassword method, hooks to hash password, to User model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,11 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require("../config/connection");
+const bcrypt = require('bcrypt');
 
 class User extends Model {
-
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
 }
 
 User.init(
@@ -44,7 +47,16 @@ User.init(
         }
     },
     {
-        // TODO add hook for password
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         freezeTableName: true,
         underscored: true,
