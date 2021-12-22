569cac6f8aa6e6a1b071e2d2aa4d6f4ad35076c2
kngurley <kngurley25@gmail.com>
2021-12-22 11:35:14 -0600

    update many-to-many relationships in index

---
+++ models/PostCuisine.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require("../config/connection");
+
+class PostCuisine extends Model {
+
+}
+
+PostCuisine.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        },
+        cuisine_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'cuisine',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        modelName: 'post_cuisine'
+    }
+);
+
+module.exports = PostCuisine;
--- models/index.js
+++ models/index.js
@@ -4,6 +4,9 @@ const Comment = require('./Comment');
 const Location = require('./Location');
 const Activity = require('./Activity');
 const Cuisine = require('./Cuisine');
+const PostLocation = require('./PostLocation');
+const PostActivity = require('./PostActivity');
+const PostCuisine = require('./PostCuisine');
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -29,8 +32,40 @@ Comment.belongsTo(Post, {
     foreignKey: 'post_id',
 });
 
+Post.belongsToMany(Location, {
+    through: PostLocation,
+    as: 'locations',
+    foreignKey: 'post_id'
+});
 
+Location.belongsToMany(Post, {
+    through: PostLocation,
+    as: 'posts',
+    foreignKey: 'location_id'
+});
 
+Post.belongsToMany(Activity, {
+    through: PostActivity,
+    as: 'activities',
+    foreignKey: 'post_id'
+});
 
+Activity.belongsToMany(Post, {
+    through: PostActivity,
+    as: 'posts',
+    foreignKey: 'activity_id'
+});
+
+Post.belongsToMany(Cuisine, {
+    through: PostCuisine,
+    as: 'cuisine',
+    foreignKey: 'post_id'
+});
+
+Cuisine.belongsToMany(Post, {
+    through: PostCuisine,
+    as: 'posts',
+    foreignKey: 'cuisine_id'
+});
 
 module.exports = { User, Post, Comment, Location, Activity, Cuisine }
