ee60a8ea042d6eb1d62417a2e38b46c8a1d1c2f0
Sam Davenport <samueldavenport@att.net>
2021-12-23 22:43:34 -0600

    updated ui. implemented handlebars.js in the server.js file. required path and added code to serve static files from the public directory. local server rendered homepage with some dynamic data from get all posts route.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -20,7 +20,7 @@ router.get('/', (req, res) => {
     })
       .then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
-  
+        console.log(dbPostData);
         res.render('homepage', 
         { posts
       }
