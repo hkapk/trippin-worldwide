35c5bf615c00994080eb4d801204018a0a728478
Christine Baxter <christine.d.baxter@live.com>
2021-12-27 20:19:40 -0600

    add map and retrieve cities

---
+++ public/javascript/location.js
@@ -0,0 +1,57 @@
+
+// const citymap = {
+//     chicago: {
+//         center: { lat: 41.878, lng: -87.629 },
+//         population: 27100000,
+//     },
+//     newyork: {
+//         center: { lat: 40.714, lng: -74.005 },
+//         population: 8405837,
+//     },
+//     losangeles: {
+//         center: { lat: 34.052, lng: -118.243 },
+//         population: 3857799,
+//     },
+//     vancouver: {
+//         center: { lat: 49.25, lng: -123.1 },
+//         population: 603502,
+//     }
+// };
+const cityData = "/api/locations";
+async function initMap() {
+    try {
+        const response = await fetch(cityData);
+        const { city, country } = await response.json();
+        return { city, country };
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+initMap().then(citymap => {
+    citymap;
+    // Create the map.
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: { lat: 37.09, lng: -95.712 },
+        mapTypeId: "terrain",
+    });
+
+    // Construct the circle for each value in citymap.
+    // Note: We scale the area of the circle based on the population.
+    for (const city in citymap) {
+        // Add the circle for this city to the map.
+        const cityCircle = new google.maps.Circle({
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.35,
+            map,
+            center: city.center,
+            radius: Math.sqrt(city.count) * 100,
+        });
+        console.log(city.count);
+    };
+});
+
