81d6907ef6b35d21f678db952b662e91c2f6872f
kngurley <kngurley25@gmail.com>
2021-12-28 14:44:28 -0600

    add dashboard handlebars and link to dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const res = require('express/lib/response');
 const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
