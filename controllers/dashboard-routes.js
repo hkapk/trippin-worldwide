f4bc4085e75432927e0d769c28847a385a70f4ba
Sam Davenport <samueldavenport@att.net>
2022-01-02 13:40:06 -0600

    eliminated some information from each post on homepage. fixed bug where the page wouldn't route to the dashboard upon signup or login

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, (req, res) => {
+router.get('/', (req, res) => {
     Post.findAll({
       where: {
         // use the ID from the session
