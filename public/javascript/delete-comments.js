5eb6b99c23f5314852af4b0740808c9ebdbce1f8
kngurley <kngurley25@gmail.com>
2022-01-05 17:53:32 -0600

    misc whitespace and formatting

--- public/javascript/delete-comments.js
+++ public/javascript/delete-comments.js
@@ -1,16 +1,16 @@
 async function deleteFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const id = document.querySelector('.delete-btn').getAttribute('data-id');
-    
-      const response = await fetch(`/api/comments/${id}`, {
-        method: 'DELETE'
-      });
-      if (response.ok) {
-        location.reload();
-    } else {
-        alert(response.statusText);
-    }
-   }
-  
-  document.querySelector('#button-bubble').addEventListener('click', deleteFormHandler);
+  const id = document.querySelector(".delete-btn").getAttribute("data-id");
+
+  const response = await fetch(`/api/comments/${id}`, {
+    method: "DELETE",
+  });
+  if (response.ok) {
+    location.reload();
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector("#button-bubble").addEventListener("click", deleteFormHandler);
